29c81a976cecd09c055c919f61b64fc34dede4bc
kamilkurach <11406583+kamilkurach@users.noreply.github.com>
2024-08-30 18:18:25 +0200

    Add #2 solution 3131

--- python/3131.py
+++ python/3131.py
@@ -2,3 +2,7 @@
 class Solution:
     def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
         return list(set([a - b for a, b in zip(sorted(nums2), sorted(nums1))])).pop(0)
+#2
+class Solution:
+    def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
+        return sorted(nums2)[0] - sorted(nums1)[0]
