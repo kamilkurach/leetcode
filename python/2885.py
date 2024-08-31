8d81bb59d2544b1cf2652f6f7ace635df7ab5273
kamilkurach <11406583+kamilkurach@users.noreply.github.com>
2024-08-31 22:56:50 +0200

    Add #2 python solution for 2885

--- python/2885.py
+++ python/2885.py
@@ -8,3 +8,15 @@ def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
         'first_name',
         'last_name',
         'age_in_years'))
+
+#2 according to leetcode scoring solution #2 is slower
+'''
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    return students.rename(columns={
+        'id':'student_id',
+        'first':'first_name',
+        'last':'last_name',
+        'age':'age_in_years'})
+'''
