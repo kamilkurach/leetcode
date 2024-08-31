106bf9582298a0653f96e0e78f901eb7771f781a
kamilkurach <11406583+kamilkurach@users.noreply.github.com>
2024-08-31 22:25:55 +0200

    Add python solution to 2882

---
+++ python/2882.py
@@ -0,0 +1,5 @@
+#2882. Drop Duplicate Rows
+import pandas as pd
+
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    return customers.drop_duplicates(subset='email', keep='first')
