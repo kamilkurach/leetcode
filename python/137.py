6de700cc89bd2fe8fe5ee87410d070e95691ac45
kamilkurach <11406583+kamilkurach@users.noreply.github.com>
2024-10-24 23:56:53 +0200

    Add python solution for 137

---
+++ python/137.py
@@ -0,0 +1,6 @@
+# 137. Single Number II
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        for element in nums:
+            if nums.count(element) == 1:
+                return element
