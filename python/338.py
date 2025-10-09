67c01cff421b60e981d2b81d35ca2cfa712c7ee6
Kamil Kurach <11406583+kamilkurach@users.noreply.github.com>
2025-10-09 11:55:19 +0200

    Add python solution for 338

---
+++ python/338.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+       result = []
+       for i in range(n + 1):
+            b = bin(i)[2:].count("1")
+            result.append(b)
+       return result
